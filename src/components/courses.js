d23d99563fdfc93bef59ad05975ddfa6861195b2
jeffnts <jf.nts10@gmail.com>
2019-10-11 14:09:28 -0300

    Courses section modifications

--- src/components/courses.js
+++ src/components/courses.js
@@ -1,8 +1,10 @@
-import React from "react"
+import React, { Fragment, useState } from "react"
 import { COLORS } from "../styles/constants"
 
 import BodyLine from "../images/bodyLine.png"
 import MissionImg from "../images/levelUP3.jpg"
+import FocusImg from "../images/LevelUp17.jpg"
+import RealTimeStrategyImg from "../images/startae-team.png"
 
 import CirComp from "../images/cirCompIcon.png"
 import BlkComp from "../images/blkCompIcon.png"
@@ -11,6 +13,8 @@ import HandIcon from "../images/handIcon.png"
 import courseStyles from "../styles/courses.module.scss"
 
 const Courses = () => {
+const [ selectedCourse, setSelectedCourse ] = useState('mission')
+
   return (
     <div id="courses" className={courseStyles.courseMain}>
     <h2
@@ -36,28 +40,89 @@ const Courses = () => {
           overflow: "visible",
         }}
       >
-        <div className={courseStyles.skewCardCourse}>
+        <div 
+          className={courseStyles.skewCardCourse}
+          style={{
+            backgroundImage: `url(${selectedCourse === 'mission'
+              ? MissionImg
+              : selectedCourse === 'focus'
+                ? FocusImg
+                : selectedCourse === 'realTimeStrategy'
+                  ? RealTimeStrategyImg
+                  : null})`,
+              
+          }}
+        >
           <div className={courseStyles.cardContCourse}>
-            <p>
-              LevelUP’s directive is to serve as a software factory to support a
-              full range of Air Force and DoD missions.
-            </p>
+            {
+              selectedCourse === 'mission'
+                && <p>
+                      <strong>LevelUP’s</strong> directive is to serve as a software factory
+                      to support a full range of Air Force and DoD missions.
+                    </p>
+            }
+
+            {
+              selectedCourse === 'focus'
+              &&  <p
+                    style={{fontSize: '22px'}}
+                  >
+                    Although our initial area of focus supports a range of Air Force missions, 
+                    LevelUP will broaden its software development services to other functional areas. 
+                    This includes a Unified Platform that will allow greater operational capabilities 
+                    between the U.S. Air Force, U.S. Army, U.S. Navy, U.S. Marines and U.S. Coast Guard.
+                  </p>
+            }
+
+            {
+            selectedCourse === 'realTimeStrategy'
+              && <p
+                  style={{fontSize: '28px'}}
+                >
+                    By developing software using agile processes and the latest DevSecOps technology, 
+                    we’re creating innovative capabilities at the scale and speed of need.
+                  </p>
+            }    
           </div>
         </div>
 
         <div className={courseStyles.skewCardBackCourse}>
           <div className={courseStyles.courses}>
-            <div className={courseStyles.courseItem}>
+            <div 
+              className={courseStyles.courseItem}
+              onClick={() => setSelectedCourse('mission')}
+            >
               <img src={BlkComp} alt="" />
               <p className={courseStyles.courseTextP}>Mission</p>
             </div>
-            <img src={BodyLine} alt="" className={courseStyles.coursesDivide} />
-            <div className={courseStyles.courseItem}>
+            <img 
+              src={BodyLine} alt="" 
+              className={courseStyles.coursesDivide}
+              style={{
+                opacity: selectedCourse === 'mission' && 1
+              }} 
+            />
+            
+            <div 
+              className={courseStyles.courseItem}
+              onClick={()=>setSelectedCourse('focus')}
+            >
               <img src={CirComp} alt="" />
               <p className={courseStyles.courseTextP}>Focus</p>
             </div>
+            <img 
+              src={BodyLine} alt="" 
+              className={courseStyles.coursesDivide}
+              style={{
+                opacity: selectedCourse === 'focus' && 1
+              }} 
+            />
+
             <div className={courseStyles.courseSpacer} />
-            <div className={courseStyles.courseItem}>
+            <div 
+              className={courseStyles.courseItem}
+              onClick={()=> setSelectedCourse('realTimeStrategy')}
+            >
               <img src={HandIcon} alt="" />
               <p className={courseStyles.courseTextP}>
                 Real Time
@@ -65,37 +130,99 @@ const Courses = () => {
                 Strategy
               </p>
             </div>
-          </div>
+            <img 
+              src={BodyLine} alt="" 
+              className={courseStyles.coursesDivide}
+              style={{
+                opacity: selectedCourse === 'realTimeStrategy' && 1
+              }} 
+            />
+          </div>          
         </div>
       </content>
     </div>
 
     <div className={courseStyles.mobBackCourses}>
       <div className={courseStyles.mobContCourses}>
-        <img src={MissionImg} alt="" />
-        <p>
-          <strong>LevelUP’s</strong> directive is to serve as a software factory
-          to support a full range of Air Force and DoD missions.
-        </p>
+        {
+          selectedCourse === 'mission'
+            && <Fragment>
+                  <img src={MissionImg} alt="" />
+                  <p>
+                    <strong>LevelUP’s</strong> directive is to serve as a software factory
+                    to support a full range of Air Force and DoD missions.
+                  </p>
+              </Fragment>
+        }
+
+        {
+          selectedCourse === 'focus'
+          && <Fragment>
+                <img src={FocusImg} alt="" />
+                <p>
+                  Although our initial area of focus supports a range of Air Force missions, 
+                  LevelUP will broaden its software development services to other functional areas. 
+                  This includes a Unified Platform that will allow greater operational capabilities 
+                  between the U.S. Air Force, U.S. Army, U.S. Navy, U.S. Marines and U.S. Coast Guard.
+                </p>
+            </Fragment>
+        }
+
+        {
+         selectedCourse === 'realTimeStrategy'
+          && <Fragment>
+                <img src={RealTimeStrategyImg} alt="" />
+                <p>
+                  By developing software using agile processes and the latest DevSecOps technology, 
+                  we’re creating innovative capabilities at the scale and speed of need.
+                </p>
+            </Fragment>
+        }    
       </div>
       <div className={courseStyles.mobSectCourses}>
         <ul>
           <li>
-            <div className={courseStyles.courseItem}>
+            <div 
+              className={courseStyles.courseItem}
+              onClick={() => setSelectedCourse('mission')}  
+            >
               <img src={BlkComp} alt="" />
               <p>Mission</p>
             </div>
           </li>
-          <img src={BodyLine} alt="" className={courseStyles.coursesDivide} />
+          <img 
+              src={BodyLine} alt="" 
+              className={courseStyles.coursesDivide}
+              style={{
+                opacity: selectedCourse === 'mission' && 1
+              }} 
+            />
+
+
           <li>
-            <div className={courseStyles.courseItem}>
+            <div 
+              className={courseStyles.courseItem}
+              onClick={() => setSelectedCourse('focus')} 
+            >
               <img src={CirComp} alt="" />
               <p>Focus</p>
             </div>
           </li>
+          <img 
+              src={BodyLine} alt="" 
+              className={courseStyles.coursesDivide}
+              style={{
+                opacity: selectedCourse === 'focus' && 1
+              }} 
+            />
+
+
           <div className={courseStyles.courseSpacer} />
           <li>
-            <div className={courseStyles.courseItem}>
+            <div 
+              className={courseStyles.courseItem}
+              onClick={() => setSelectedCourse('realTimeStrategy')} 
+            >
               <img src={HandIcon} alt="" />
               <p>
                 Real Time
@@ -104,6 +231,14 @@ const Courses = () => {
               </p>
             </div>
           </li>
+          <img 
+              src={BodyLine} alt="" 
+              className={courseStyles.coursesDivide}
+              style={{
+                opacity: selectedCourse === 'realTimeStrategy' && 1
+              }} 
+            />
+
         </ul>
       </div>
     </div>
